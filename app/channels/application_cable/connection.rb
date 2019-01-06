3030aa148c9550a6f6664caa717ba5e0d94b7b1d
vitalyliber <zenamax@gmail.com>
2019-01-06 17:56:44 +0300

    auth method was moved to helper

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,5 +1,6 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    include ApplicationHelper
     identified_by :current_user
 
     def connect
@@ -11,21 +12,8 @@ module ApplicationCable
     def find_verified_user
       logger.debug 'Connection debug info:'
       logger.debug "Bearer: #{cookies[:bearer]}"
-      err, response = RestClient.get(
-          "#{ENV.fetch('API_SERVER') {'https://app-server.com'}}/users/api_auth",
-          {Authorization: "Bearer #{cookies[:bearer]}"}) {|response, request, result, &block|
-        case response.code
-        when 200
-          [false, response]
-        else
-          [true, response]
-        end
-      }
-      json = JSON.parse response.body
-      if not err
-        user = User.find_or_create_by(external_key: json['user']['external_key'])
-        user.update(name: json['user']['name']) if user.name != json['user']['name']
-        user.update(avatar_url: json['user']['avatar_url']) if user.avatar_url != json['user']['avatar_url']
+      user = auth(cookies[:bearer])
+      if user.present?
         user
       else
         reject_unauthorized_connection
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def auth(bearer)
+    err, response = RestClient.get(
+        "#{ENV.fetch('API_SERVER') {'https://app-server.com'}}/users/api_auth",
+        {Authorization: "Bearer #{bearer}"}) {|response, request, result, &block|
+      case response.code
+      when 200
+        [false, response]
+      else
+        [true, response]
+      end
+    }
+    json = JSON.parse response.body
+    if not err
+      user = User.find_or_create_by(external_key: json['user']['external_key'])
+      user.update(name: json['user']['name']) if user.name != json['user']['name']
+      user.update(avatar_url: json['user']['avatar_url']) if user.avatar_url != json['user']['avatar_url']
+      user
+    else
+      nil
+    end
+  end
 end
